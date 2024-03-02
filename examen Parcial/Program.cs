9240adc83c5576f777740517a35b483e9d69a0b2
Mynor-Alonso-1992 <136778116+Mynor-Alonso-1992@users.noreply.github.com>
2024-03-02 10:57:51 -0600

    Add files via upload

---
+++ examen Parcial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Calculadora
+{
+    public static int Sumar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int Restar(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public static int Multiplicar(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static int Dividir(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+
+    static void Main(string[] args)
+    {
+        // Pedir dos números al usuario
+        Console.WriteLine("Introduce el primer número:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        // Invocar los métodos
+        int suma = Sumar(num1, num2);
+        int resta = Restar(num1, num2);
+        int multiplicacion = Multiplicar(num1, num2);
+        int division = Dividir(num1, num2);
+
+        // Mostrar los resultados
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
+}
